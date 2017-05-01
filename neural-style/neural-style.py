43e947f99ef0175d0fdf3412600022f944dc4fde
rabierre <rabierre@gmail.com>
2017-05-02 05:46:33 +0900

    다른 코드 참고해서 재작성함

--- neural-style/neural-style.py
+++ neural-style/neural-style.py
@@ -1,86 +1,78 @@
 import tensorflow as tf
+import numpy as np
+import scipy.misc as sm
+import vgg19
 
-import vgg16
+VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
+CONTENT_LAYER = 'relu4_2'
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+BATCH_SIZE = 500
 
-flags = tf.app.flags
-FLAGS = flags.FLAGS
 
-flags.DEFINE_float('learning_rate', 1e+0, 'Initial learning rate.')
-flags.DEFINE_integer('max_steps', 500, 'Number of steps to run trainer.')
+def read_image(path):
+    return sm.imread(path).astype(np.float)
 
-contents_queue = tf.train.string_input_producer(['cat.jpg', 'dog.jpg']) # List of contents image file
-style_queue = tf.train.string_input_producer(['style.jpg']) # list of files to read
-
-reader = tf.WholeFileReader()
-
-filename, filecontents = reader.read(contents_queue)
-content_images = tf.image.decode_jpeg(filecontents, channels=3) # use png or jpg decoder based on your files.
-
-filename, filecontents = reader.read(style_queue)
-style_images = tf.image.decode_jpeg(filecontents, channels=3)
-
-resized_contents = tf.image.resize_images(content_images, [224,224]) / 255
-resized_styles = tf.image.resize_images(style_images, [224,224]) / 255
-
-min_after_dequeue = 100
-batch_size = 1
-capacity = min_after_dequeue + 3 * batch_size
-content_batch, style_batch = tf.train.shuffle_batch(
-    [resized_contents, resized_styles], batch_size=batch_size, capacity=capacity,
-    min_after_dequeue=min_after_dequeue)
-
-content_vgg = vgg16.Vgg16()
-content_vgg.build(content_batch)
-style_vgg = vgg16.Vgg16()
-style_vgg.build(style_batch)
-
-x = tf.Variable(tf.random_uniform([1, 224, 224, 3], minval=0, maxval=255, dtype=tf.float32))
-variable_vgg = vgg16.Vgg16()
-variable_vgg.build(x / 255)
-
-result_image = tf.image.encode_png(tf.cast(x[0], tf.uint8))
-write_image = tf.write_file("img/output.png", result_image)
-
-content_loss = tf.nn.l2_loss(variable_vgg.conv4_2 - content_vgg.conv4_2)
+def save_image(image, path):
+    image = np.clip(image, 0, 255).astype(np.uint8)
+    sm.imsave(path, image)
 
 def get_layer_style_loss(variable_layer, style_layer):
     shape = variable_layer.get_shape().as_list()
     N = shape[3]
     M = shape[1] * shape[2]
-    reshaped_variable = tf.reshape(variable_layer, [-1, N])
-    G = tf.matmul(reshaped_variable, reshaped_variable, transpose_a=True, transpose_b=False)
-    reshaped_style = tf.reshape(style_layer, [-1, N])
-    A = tf.matmul(reshaped_style, reshaped_style, transpose_a=True, transpose_b=False)
-    return tf.nn.l2_loss(G - A) / (2 * N**2 * M ** 2)
+    reshaped_variable = tf.reshape(variable_layer, (-1, N))
+    G = tf.matmul(reshaped_variable, reshaped_variable, transpose_a=True)
+    reshaped_style = tf.reshape(style_layer, (-1, N))
+    A = tf.matmul(reshaped_style, reshaped_style, transpose_a=True)
+    return tf.nn.l2_loss(G - A) / (4.0 * N ** 2 * M ** 2)
 
-E1_1 = get_layer_style_loss(variable_vgg.conv1_1, style_vgg.conv1_1)
-E2_1 = get_layer_style_loss(variable_vgg.conv2_1, style_vgg.conv2_1)
-E3_1 = get_layer_style_loss(variable_vgg.conv3_1, style_vgg.conv3_1)
-E4_1 = get_layer_style_loss(variable_vgg.conv4_1, style_vgg.conv4_1)
-E5_1 = get_layer_style_loss(variable_vgg.conv5_1, style_vgg.conv5_1)
+contentImg = read_image("state.jpg")
+styleImg   = read_image("style.jpg")
+contentImg = tf.reshape(tf.image.resize_images(contentImg, [448, 448]), [-1, 448, 448, 3])
+styleImg = tf.reshape(tf.image.resize_images(styleImg, [448, 448]), [-1, 448, 448, 3])
 
-style_loss = (E1_1 + E2_1 + E3_1 + E4_1 + E5_1) / 5
-
-alpha, beta = 1 / 1000, 1
-total_loss = alpha * content_loss + beta * style_loss
-
-train_op = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(total_loss)
-
-init_op = tf.global_variables_initializer()
+shape = contentImg.get_shape().as_list()
+content_features = {}
+style_features = {}
 
 with tf.Session() as sess:
-    sess.run(init_op)
+    img = tf.placeholder('float', shape=shape)
+    net, mean_pixel = vgg19.net(VGG_PATH, img)
 
-    # Start populating the filename queue.
+    ### calc vgg layers of content and style
+    content_pre = vgg19.preprocess(contentImg, mean_pixel)
+    content_features[CONTENT_LAYER] = net[CONTENT_LAYER].eval(feed_dict={img: content_pre.eval()})
+    style_pre = vgg19.preprocess(styleImg, mean_pixel)
+    for layer in STYLE_LAYERS:
+        style_features[layer] = net[layer].eval(feed_dict={img: style_pre.eval()})
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+    ### Image to be trained
+    noise = np.random.normal(size=shape, scale=np.std(contentImg.eval()) * 0.1)
+    init = content_pre * (1 - 0.0) + noise * 0.0
+    image = tf.Variable(init, dtype='float')
+    net, _ = vgg19.net(VGG_PATH, image)
 
-    for i in range(FLAGS.max_steps):
+### loss operators
+content_loss = tf.nn.l2_loss(net[CONTENT_LAYER] - content_features[CONTENT_LAYER])
+
+style_loss = 0.0
+for layer in STYLE_LAYERS:
+    style_loss += get_layer_style_loss(net[layer], style_features[layer])
+style_loss /= 5.0
+
+ALPHA, BETA = 1.0 , 1e4
+total_loss = ALPHA * content_loss + BETA * style_loss
+
+### training operator
+global_step = tf.Variable(0, trainable=False)
+learning_rate = tf.train.exponential_decay(2e1, global_step, 10, 0.94, staircase=True)
+train_op = tf.train.AdamOptimizer(learning_rate).minimize(total_loss, global_step=global_step)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(BATCH_SIZE):
         sess.run(train_op)
-        print(i, " loss : ", total_loss.eval())
-
-    sess.run(write_image)
-
-    coord.request_stop()
-    coord.join(threads)
+        print("{0}th loss: {1}".format(i, total_loss.eval()))
+        if (i % 10 == 0):
+            save_image(vgg19.unprocess(image.eval().reshape(shape[1:]), mean_pixel), 'output%03d.jpg' % i)
+    save_image(vgg19.unprocess(image.eval().reshape(shape[1:]), mean_pixel), 'output.jpg')
