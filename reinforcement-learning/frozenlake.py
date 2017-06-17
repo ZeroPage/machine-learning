91a6d933102c4528ac49b52fc319596bb884aab7
rabierre <rabierre@gmail.com>
2017-06-18 04:14:13 +0900

    Deterministic frozenlake

--- reinforcement-learning/frozenlake.py
+++ reinforcement-learning/frozenlake.py
@@ -3,9 +3,11 @@ import numpy as np
 import random
 from gym.envs.registration import register
 import gym.envs.toy_text
+import matplotlib.pyplot as plt
 
-epoch = 1000
+epoch = 2000
 dis = 0.9
+alpha = .085
 
 register(
    	id='FrozenLake-v1',
@@ -29,18 +31,17 @@ for i in range(epoch):
 	reward = 0
 
 	while not done:
-		# action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) // (i+1))
-		action = rargmax(Q[state, :])
+		action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) // (i+1))
 
 		new_state, reward, done, _ = env.step(action)
-		Q[state, action] = reward + dis * np.max(Q[new_state, :])
+		Q[state, action] = (1-alpha) * Q[state, action] + alpha * (reward + dis * np.max(Q[new_state, :]))
 
 		state = new_state
 		total_reward += reward
 
 	reward_list.append(total_reward)
-	if i >= 1990:
-		print(reward)
 
 print(Q)
 print("Success rate: " + str(sum(reward_list)/epoch * 100))
+plt.bar(range(len(reward_list)), reward_list, color="blue")
+plt.show()
